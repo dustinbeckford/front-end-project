37ff7a2c7797510435e5b1e3876cb1bca2753e6f
Dustin Beckford <61157341+xJamoni@users.noreply.github.com>
2023-05-14 23:46:43 -0400

    readme.md created

--- index.js
+++ index.js
@@ -6,54 +6,87 @@ async function searchForVehicle() {
   const searchMake = document.querySelector("#searchmake").value;
   const searchModel = document.querySelector("#searchmodel").value;
   const searchYear = document.querySelector("#searchyear").value;
+  
+  // clears inner text so that it doesn't stack subsequent requests
   carContainer.innerText = "";
-  const carResults = await fetch(
-    `https://api.api-ninjas.com/v1/cars?limit=2&model=${searchModel}&make=${searchMake}&year=${searchYear}`,
-    {
-      method: "GET",
-      headers: {
-        "X-Api-Key": "gAUaIgs/cTbSFFQiiYAl3A==PKXn6hFVATNOWhgO",
-        "Content-Type": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-    }
-  );
-  const json = await carResults.json();
-  json.forEach((car) => {
-    console.log(car.fuel_type);
 
+  const url = `https://api.api-ninjas.com/v1/cars?limit=2&model=${searchModel}&make=${searchMake}&year=${searchYear}`;
+  const opts = {
+    method: "GET",
+    headers: {
+      "X-Api-Key": "gAUaIgs/cTbSFFQiiYAl3A==PKXn6hFVATNOWhgO",
+      "Content-Type": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  };
+  const carResults = await fetch(url, opts);
+  const json = await carResults.json();
+
+  if (!json.error && json.length > 0) {
+    const car = json[0]; 
     const carInfo = document.createElement("div");
     carInfo.className = "car-container-div"
+
+    // image
     const carImage = document.createElement("img");
     carImage.src = "";
     carImage.src = `https://source.unsplash.com/random/?${searchMake},${searchModel}`;
     carImage.className = "car-image"
-    const carYear = document.createElement("h3");
+
+    // year
+    const carYear = document.createElement("h5");
     carYear.innerText = `Car Year: ${car.year}`;
-    const carMake = document.createElement("h3");
+
+    // make
+    const carMake = document.createElement("h5");
     carMake.innerText = `Car Make: ${car.make}`;
-    const carModel = document.createElement("h3");
+    
+    // model
+    const carModel = document.createElement("h5");
     carModel.innerText = `Car Mode: ${car.model}`;
-    const carFuelType = document.createElement("h3");
-    carFuelType.innerText = `Car Fuel Type:${car.fuel_type}`;
-    const carTransmisson = document.createElement("h3");
-    carTransmisson.innerText = `Car Transmission: ${car.transmission}`;
-    const cardrive = document.createElement("h3");
-    cardrive.innerText = `Car Drive:  ${car.drive}`;
-    const carCityMpg = document.createElement("h3");
+
+    // fuel type
+    const carFuelType = document.createElement("h5");
+    carFuelType.innerText = `Car Fuel Type: ${car.fuel_type}`;
+
+    // trans
+    const carTransmisson = document.createElement("h5");
+    var transText = "Automatic";
+    if (car.transmission.toLowerCase() !== "a") {
+      transText = "Manual";
+    }
+    carTransmisson.innerText = `Car Transmission: ${transText}`;
+
+    // drive
+    const cardrive = document.createElement("h5");
+    cardrive.innerText = `Car Drive:  ${car.drive.toUpperCase()}`;
+
+    // city mpg
+    const carCityMpg = document.createElement("h5");
     carCityMpg.innerText = `City MPG:  ${car.city_mpg}`;
-    const carHighwayMpg = document.createElement("h3");
+
+    // highway mpg
+    const carHighwayMpg = document.createElement("h5");
     carHighwayMpg.innerText = `Highway MPG:  ${car.highway_mpg}`;
-    const carComboMpg = document.createElement("h3");
+
+    // combo mpg
+    const carComboMpg = document.createElement("h5");
     carComboMpg.innerText = `Total MPG:  ${car.combination_mpg}`;
-    const carClass = document.createElement("h3");
+
+    // class
+    const carClass = document.createElement("h5");
     carClass.innerText = `Car Class:  ${car.class}`;
-    const carCylinders = document.createElement("h3");
+
+    // cylinders
+    const carCylinders = document.createElement("h5");
     carCylinders.innerText = `Total Cylinders: ${car.cylinders}`;
-    const carDisplacement = document.createElement("h3");
+
+    // displacement
+    const carDisplacement = document.createElement("h5");
     carDisplacement.innerText = `Displacement in Liters:  ${car.displacement}`;
+
     // Appending Varibles to Div Created Above
     carInfo.append(
       carYear,
@@ -70,7 +103,17 @@ async function searchForVehicle() {
       carClass,
       carImage
     );
-    //Appending Big container made on HTML to Div with Varibles Created Above
+
+    // Appending Big container made on HTML to Div with Varibles Created Above
     carContainer.append(carInfo);
-  });
+  } else {
+    const errorElement = document.createElement("h2");
+    if (json.length === 0) {
+      errorElement.innerText = "No results found";
+    } else {
+      errorElement.innerText = `Error: ${json.error}`;
+    }
+    carContainer.append(errorElement);
+  }
+
 }
